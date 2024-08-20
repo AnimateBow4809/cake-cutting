23652c21b6bf5af0ac0882e92101b00279d6fa84
AnimateBow4809 <alidashtbozorg709@gmail.com>
2024-08-20 22:47:19 +0330

    finished

--- src/main/java/bow/animate/CakeCutting.java
+++ src/main/java/bow/animate/CakeCutting.java
@@ -1,49 +1,65 @@
 package bow.animate;
+
 import java.util.*;
+
+import java.util.Scanner;
 
 public class CakeCutting {
 
-
-    public static int getMaxPieceArea(TreeSet<Integer> cuts) {
-        int prevCut = cuts.first();
-        int maxPieceArea = 0;
-
-        for (int cut : cuts.tailSet(prevCut, false)) {
-            maxPieceArea = Math.max(maxPieceArea, cut - prevCut);
-            prevCut = cut;
+    /*
+        calculate in O(n) time
+        and O(max(H,W)) space complexity
+     */
+    public static long calcMax(long w, long h, boolean[] xAxisCuts, boolean[] yAxisCuts) {
+        long x=0,y=0;
+        long xMax=0,yMax=0;
+        for (int i = 0; i < w+1; i++) {
+            if (i==0){
+                x++;
+                continue;
+            }
+            if (xAxisCuts[i]){
+                xMax=Math.max(xMax,x);
+                x=0;
+            }
+            x++;
         }
 
-        return maxPieceArea;
+        for (int i = 0; i < h+1; i++) {
+            if (i==0){
+                y++;
+                continue;
+            }
+            if (yAxisCuts[i]){
+                yMax=Math.max(yMax,y);
+                y=0;
+            }
+            y++;
+        }
+        return xMax*yMax;
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int W = scanner.nextInt();
-        int H = scanner.nextInt();
-        int q = scanner.nextInt();
-
-        TreeSet<Integer> horizontalCuts = new TreeSet<>();
-        TreeSet<Integer> verticalCuts = new TreeSet<>();
-        horizontalCuts.add(0);
-        horizontalCuts.add(H);
-        verticalCuts.add(0);
-        verticalCuts.add(W);
-
-        int maxArea = W * H;
-        for (int i = 0; i < q; i++) {
-            char cutType = scanner.next().charAt(0);
-            int coord = scanner.nextInt();
-
-            if (cutType == 'h') {
-                horizontalCuts.add(coord);
-            } else {
-                verticalCuts.add(coord);
+        String[] input=scanner.nextLine().split(" ");
+        int w = Integer.parseInt(input[0]);
+        int h = Integer.parseInt(input[1]);
+        int q = Integer.parseInt(input[2]);
+        boolean[] xAxisCuts=new boolean[w+1];
+        boolean[] yAxisCuts=new boolean[h+1];
+        xAxisCuts[w]=true;
+        yAxisCuts[h]=true;
+        long max=w*h;
+        for (long i = 0; i < q; i++) {
+            input=scanner.nextLine().split(" ");
+            if (input[0].toUpperCase(Locale.ROOT).equals("V")){
+                xAxisCuts[Integer.parseInt(input[1])]=true;
+            }else {
+                yAxisCuts[Integer.parseInt(input[1])]=true;
             }
-
-            maxArea = Math.max(maxArea, getMaxPieceArea(horizontalCuts) * getMaxPieceArea(verticalCuts));
-            System.out.println(maxArea);
         }
+        max=calcMax(w,h,xAxisCuts,yAxisCuts);
+        System.out.println(max);
     }
+
 }
